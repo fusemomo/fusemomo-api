d7124103e09b1cfa2e3a92995e63b8b8c98e20dc
Kshitiz_Mhto <kshitizmhto101@gmail.com>
2026-02-25 09:53:49 +0545

    feat: added routes for ListAllAPIKeysHandler

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -39,6 +39,7 @@ func (s *FiberServer) registerAPIv1Routes(h *v1.Handler) {
 
 	apiKey := app.Group("/key")
 	apiKey.Get("/", h.GetAPIKeysHandler)
+	apiKey.Get("/all", h.ListAllAPIKeysHandler)
 	apiKey.Post("/create", h.CreateAPIkeysForAgentsHandler)
 	apiKey.Post("/delete", h.DeleteAPIkeysForAgentsHandler)
 	apiKey.Post("/sync-expired", h.SyncExpiredAPIKeysHandler)

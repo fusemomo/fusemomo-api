16c76393926c2d4ff651c6e4b812347e8088f30e
Kshitiz_Mhto <kshitizmhto101@gmail.com>
2026-03-25 07:20:43 +0545

    feat: added GC loop and sessionstore in server

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,8 +1,11 @@
 package server
 
 import (
+	"context"
+
 	_ "fusemomo-api/cmd/api/docs"
 	"fusemomo-api/internal/middlewares/ratelimit"
+	"fusemomo-api/internal/session"
 	"fusemomo-api/internal/utils"
 
 	"github.com/gofiber/fiber/v3"
@@ -11,20 +14,26 @@ import (
 
 type FiberServer struct {
 	*fiber.App
-	DB          *pgxpool.Pool
-	RateLimiter *ratelimit.RateLimiter
+	DB           *pgxpool.Pool
+	RateLimiter  *ratelimit.RateLimiter
+	SessionStore *session.Store
 }
 
 func New(db *pgxpool.Pool) *FiberServer {
+	sessStore := session.New()
+
+	// Start background GC loop that purges expired sessions every 5 min.
+	go sessStore.GCLoop(context.Background())
+
 	server := &FiberServer{
 		App: fiber.New(fiber.Config{
 			ServerHeader: "fusemomo-api",
 			AppName:      "fusemomo-api",
 			ErrorHandler: utils.ErrorHandler,
 		}),
-		DB:          db,
-		RateLimiter: ratelimit.NewRateLimiter(ratelimit.DefaultConfig()),
+		DB:           db,
+		RateLimiter:  ratelimit.NewRateLimiter(ratelimit.DefaultConfig()),
+		SessionStore: sessStore,
 	}
 	return server
 }
-

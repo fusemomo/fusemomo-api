908bf9fea5bd639a777ad61636d283ca072e31d3
Kshitiz_Mhto <kshitizmhto101@gmail.com>
2026-03-28 02:47:48 +0545

    feat: added env util function for GetEnvAsBool

--- internal/utils/env.go
+++ internal/utils/env.go
@@ -18,6 +18,17 @@ func GetEnv(key, fallback string) string {
 	return fallback
 }
 
+func GetEnvAsBool(key string, fallback bool) bool {
+	if value, ok := os.LookupEnv(key); ok {
+		b, err := strconv.ParseBool(value)
+		if err != nil {
+			return fallback
+		}
+		return b
+	}
+	return fallback
+}
+
 func GetEnvAsInt(key string, fallback int) int {
 	if value, ok := os.LookupEnv(key); ok {
 		i, err := strconv.Atoi(value)
